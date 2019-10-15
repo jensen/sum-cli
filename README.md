ad85165c8d00a92cfa2ed1f85e7dd9437292e7f1
Karl Jensen <karljensen@gmail.com>
2019-10-15 18:46:46 +0000

    Added the ability to retrieve arguments
    - Created separated function to getArguments
    - Updated the README with the problem steps

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## Problem Statement
 
 > Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. If any argument is not a whole number, skip it. Do support negative numbers though.
+
+## Problem Steps
+
+- Collect user arguments
+- Iterate over each argument (L - loop)
+  - Check if number is whole or not (C in conditionals)
+    - if whole: Add two numbers together
+    - else: Skip the argument
+- Print out the resulting sum
---
+++ src/sum-cli.js
@@ -0,0 +1,18 @@
+/*
+  Write a node program that takes in an unlimited number
+  of command line arguments, goes through each and prints
+  out the sum of them. If any argument is not a whole
+  number, skip it. Do support negative numbers though.
+*/
+
+// Collect user arguments
+const getArguments = function() {
+  return process.argv.slice(2);
+};
+
+console.log(getArguments());
+// Iterate over each argument (L - loop)
+// Check if number is whole or not (C in conditionals)
+// if whole: Add two numbers together
+// else: Skip the argument
+// Print out the resulting sum
